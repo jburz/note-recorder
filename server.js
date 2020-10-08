98f949296ff2973a37a8186c319d3501203cc301
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-07 19:09:25 -0600

    server GET request will read db.json and return all saved notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const db = require("./db.json");
 
 //express app setup
 const app = express();
@@ -12,30 +13,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./assets")));
 
+//ajax request handling
+app.post("/api/notes", (req, res) => {
+    db.push(req.body);
+    console.log(db);
+    // fs.appendFile("db.json", myJSON, function(err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    // }); 
+});
+
+app.get("/api/notes", function(req, res) {
+    fs.readFile("db.json", "utf8", function(err, data) {
+        if (err) {
+            return err;
+        }
+        const response = JSON.parse(data);
+        res.send(response);
+        
+    })
+});
+
 //routing routines to serve pages from ajax requests
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/notes.html"));
-    console.log('notes');
 });
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
-    console.log('home');
 });
 
-//ajax request handling
-let data = [];
-app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    data.push(req.body);
-    const myJSON = JSON.stringify(data);
-    console.log(myJSON);
-    fs.writeFile("db.json", myJSON, function(err) {
-        if (err) {
-            return console.log(err);
-        }
-    }); 
-});
 
 //server listening on port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
